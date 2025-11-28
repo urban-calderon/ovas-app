b181a4148398910ee75796e1fea79bd63fa71dab
urban-calderon <urbanocalderon@outlook.com>
2025-11-28 11:26:54 -0600

    feat: add router with the application routes defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroductionPageComponent } from './pages/introduction/introduction.component';
+import { ObjetivesPageComponent } from './pages/objetives/objetives.component';
+import { ContentPageComponent } from './pages/content/content.component';
+import { ActivityPageComponent } from './pages/activity/activity.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/introduccion',
+    pathMatch: 'full'
+  },
+  {
+    path: 'introduccion',
+    component: IntroductionPageComponent
+  },
+  {
+    path: 'objetivos',
+    component: ObjetivesPageComponent
+  },
+  {
+    path: 'contenido',
+    component: ContentPageComponent
+  },
+  {
+    path: 'actividad',
+    component: ActivityPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'introduccion'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
