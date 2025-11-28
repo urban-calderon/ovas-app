7a7c93b8dfb7eebec9828bf55e134345b46285a4
urban-calderon <urbanocalderon@outlook.com>
2025-11-28 11:27:43 -0600

    feat: main application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionPageComponent } from './pages/introduction/introduction.component';
+import { ObjetivesPageComponent } from './pages/objetives/objetives.component';
+import { ContentPageComponent } from './pages/content/content.component';
+import { ActivityPageComponent } from './pages/activity/activity.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MultipleChoiceQuestionComponent } from './components/multiple-choice-question/multiple-choice-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionPageComponent,
+    ObjetivesPageComponent,
+    ContentPageComponent,
+    ActivityPageComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    MultipleChoiceQuestionComponent,
   ],
   imports: [
     BrowserModule,
